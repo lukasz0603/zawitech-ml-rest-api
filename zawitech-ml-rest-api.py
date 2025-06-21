18ccae471a38ee31d605af8585469898b255d689
lukasz0603 <84631919+lukasz0603@users.noreply.github.com>
2025-06-21 20:01:44 +0200

    Add files via upload

---
+++ zawitech-ml-rest-api.py
@@ -0,0 +1,30 @@
+# app.py
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["GET"])
+def predict_rain():
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": 52.23,  # Warszawa
+        "longitude": 21.01,
+        "hourly": "precipitation_probability",
+        "forecast_days": 1
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    hourly = data['hourly']
+
+    prediction = []
+    for time, prob in zip(hourly["time"], hourly["precipitation_probability"]):
+        risk = "WYSOKIE" if prob > 50 else "NISKIE"
+        prediction.append({
+            "czas": time,
+            "szansa_opadu": prob,
+            "ryzyko": risk
+        })
+
+    return jsonify(prediction)
